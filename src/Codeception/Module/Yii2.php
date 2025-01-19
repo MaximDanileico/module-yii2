bfb07e8a2d56815308055f4cb7a008a74a65a952
MaximDanileico <mdanileicowork@gmail.com>
2025-01-19 02:15:04 +0200

    added codeception/lib-innerbrowser

--- src/Codeception/Module/Yii2.php
+++ src/Codeception/Module/Yii2.php
@@ -11,7 +11,6 @@ use Codeception\Lib\Connector\Yii2\TransactionForcer;
 use Codeception\Lib\Framework;
 use Codeception\Lib\Interfaces\ActiveRecord;
 use Codeception\Lib\Interfaces\MultiSession;
-use Codeception\Lib\Interfaces\ORM;
 use Codeception\Lib\Interfaces\PartedModule;
 use Codeception\TestInterface;
 use Symfony\Component\DomCrawler\Crawler as SymfonyCrawler;
@@ -167,7 +166,7 @@ use yii\web\IdentityInterface;
  * Stability: **stable**
  *
  */
-class Yii2 extends Framework implements ActiveRecord, MultiSession, PartedModule, ORM
+class Yii2 extends Framework implements ActiveRecord, MultiSession, PartedModule
 {
     /**
      * Application config file must be set.
